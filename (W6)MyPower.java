937f767644f92e7229ec5c02fb3d358308c501cd
Kasem <32092426+kasem6746@users.noreply.github.com>
2018-01-07 15:19:13 +0700

    Add files via upload

---
+++ (W6)MyPower.java
@@ -0,0 +1,27 @@
+/**
+@Kasem_Sukumratanaporn 5907101023
+ * */
+package th.ac.utcc.cpe.kasem;
+
+public class MyPower {
+	public static void main(String[] args){
+		long x =  3, y= 18;
+		System.out.println("Pow("+x+","+y+")= "+myPower(x,y));
+		}
+		public static long myPower(long x , long y){
+		System.out.println("x="+x+",y="+y);
+		long ans = x;
+		if (y !=1 ){
+		 if (y%2 != 0){
+			 ans= x*myPower(x*x,y/2);
+		 System.out.println("Returning "+ans+",X="+x+",Y="+y+" // Y is odd");}
+		 else{ ans= myPower(x*x,y/2); // y is odd
+		System.out.println("Returning "+ans+",X="+x+",Y="+y+ " // Y is even");}
+		return ans ;
+		}
+		else {
+		System.out.println("Returning "+ans+",X="+x+",Y="+y);
+		return ans;
+		}
+		}
+}
